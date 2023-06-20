0ccf8bca831b67d548d553ef24cdffceb44b4ee5
Dimitris Georgakopoulos <dimitris.geo@gmail.com>
2023-06-20 23:32:06 +0300

    Fetched and rendered weather forecast on city name click for the next 24 hours

--- src/components/CityWeather.jsx
+++ src/components/CityWeather.jsx
@@ -1,6 +1,31 @@
-const CityWeather = () => {
-  return (
-    <div>CityWeather</div>
-  )
+import axios from "axios"
+import { useEffect, useState } from "react"
+const apiKey = import.meta.env.VITE_OPEN_WEATHER_API_KEY
+
+const CityWeather = ({lat, lon}) => {
+    const [forecastList, setForecastList] = useState([])
+
+    useEffect(() => {
+        axios
+          .get(
+            `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=8&appid=${apiKey}`
+            // `https://api.openweathermap.org/data/2.5/forecast?lat=57&lon=-2.15&cnt=3&appid=${apiKey}`
+          )
+          .then((response) => {
+            console.log(response)
+            setForecastList(response.data.list)
+          })
+          .catch((error) => console.log("My error", error));
+    }, [])
+
+    return (
+        <div>
+            {forecastList.map(weather => {
+                return (
+                    <div>{weather.weather[0].main}</div>
+                )
+            })}
+        </div>
+    )
 }
 export default CityWeather
