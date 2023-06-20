f1ff43939c43afc95aba420e15ee5f5ec61c6cdf
Dimitris Georgakopoulos <dimitris.geo@gmail.com>
2023-06-21 01:53:34 +0300

    Showing only today's weather

--- src/components/CityWeather.jsx
+++ src/components/CityWeather.jsx
@@ -13,16 +13,23 @@ const CityWeather = ({lat, lon}) => {
           )
           .then((response) => {
             console.log(response)
-            setForecastList(response.data.list)
+            console.log(response.data.list)
+            const today = new Date()
+            console.log("today.getDate()", today.getDate());
+            let list = response.data.list.filter(item => {
+                return item.dt_txt.substr(8,2) == today.getDate()
+                // console.log( item.dt_txt.substr(8,2) )
+            })
+            setForecastList(list)
           })
           .catch((error) => console.log("My error", error));
     }, [])
 
     return (
         <div>
-            {forecastList.map(weather => {
+            {forecastList && forecastList.map(weather => {
                 return (
-                  <div key={weather.dt}>
+                  <div key={weather.dt} className="d-flex align-center">
                     <h5>{new Date(weather.dt * 1000).toLocaleTimeString()}</h5>
                     <img
                       src={`https://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`}
