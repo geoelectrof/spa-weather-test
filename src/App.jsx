a62d3ad152cd982cdc97f1449b67de1c48c09fd7
Dimitris Georgakopoulos <dimitris.geo@gmail.com>
2023-06-20 20:20:11 +0300

    Created search bar, fetched data from geocoding api and rendered the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,57 @@
 import './App.css'
-import { Container, Row, Col } from 'react-bootstrap'
+import { useState } from 'react'
+import { Container, Row, Col, Form } from 'react-bootstrap'
+import axios from 'axios'
+import { useEffect } from 'react'
 
 const apiKey = import.meta.env.VITE_OPEN_WEATHER_API_KEY
 
-function App() {
 
-  console.log(apiKey)
+function App() {
+  const [cities, setCities] = useState([{name: "athens"}, {name:"thessaloniki"}])
+  const [searchText, setSearchText] = useState("")
+
+  // useEffect(() => {
+  //   axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${apiKey}`)
+  //   .then(response => {
+  //     console.log(response.data)
+  //     setCities(response.data)
+  //   })
+  // },[])
+
+  function fetchData(city){
+    axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`)
+      .then(response => {
+        setCities(response.data)
+      })
+  }
+
+  function handleChange(e){
+    setSearchText(e.target.value)
+    fetchData(e.target.value)
+  }
+  console.log(cities)
+
   return (
     <>
       <Container>
         <Row>
           <Col>
-            <h1>Test</h1>
+            <Form.Control size="lg" type="text" placeholder="Search for city" onChange={(e) => handleChange(e)}/>
           </Col>
+        </Row>
+        <Row>
+          {cities && cities.map((city, index) => {
+            return (
+              <h1 key={index}>
+                {city.name} {city.state} {city.country}
+              </h1>
+            );
+          })}
         </Row>
       </Container>
     </>
-  )
+  );
 }
 
 export default App
